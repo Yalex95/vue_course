04b3c0e6e4f8eee14254236f5c2965562335865e
yerish14 <yerisagui@gmail.com>
2023-12-27 15:05:10 -0800

    Add components

---
+++ src/components/PostCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="post-card">
+    <h3>Titulo</h3>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos soluta
+      doloremque eaque placeat, non expedita, minima laborum reprehenderit
+      voluptatum ipsum sequi dignissimos suscipit repellendus dolorem ullam
+      autem earum, architecto labore!
+    </p>
+    <PostButton />
+  </div>
+</template>
+
+<script lang="ts">
+import PostButton from "./PostButton.vue";
+
+export default {
+  name: "PostCard",
+  components: {
+    PostButton: PostButton,
+  },
+};
+</script>
+<style scoped>
+.post-card {
+  border-radius: 15px;
+  padding: 10px;
+  margin: 5px;
+  border: 1px solid #ccc;
+}
+</style>
---
+++ src/components/PostContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="post-container">
+    <PostCard />
+  </div>
+</template>
+<script lang="ts">
+import PostCard from "./PostCard.vue";
+
+export default {
+  name: "PostContainer",
+  components: {
+    PostCard: PostCard,
+  },
+};
+</script>
+<style>
+.post-container {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
